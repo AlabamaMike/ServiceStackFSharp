ac84f8b0bf74134dde396c5c06cc963681893083
Kunjan <kunjee17@gmail.com>
2014-01-04 19:03:31 +0530

    servicestack with asp.net hosting

---
+++ ServiceStackAspNetHost/ServiceStackAspNetHost/App_Start/AppHost.fs
@@ -0,0 +1,22 @@
+﻿namespace ServiceStackAspNetHost.App_Start
+    open ServiceStack
+    open ServiceStack.Common
+    open ServiceStack.WebHost.Endpoints
+    open ServiceStackAspNetHost
+    open System
+
+    //[<assembly: WebActivator.PreApplicationStartMethod(typeof<ServiceStackAspNetHost.App_Start.AppHost>, "Start")>]
+    
+    
+    type AppHost() = 
+        inherit AppHostHttpListenerBase("Hello F# Service", 
+                                        typeof<HelloService>.Assembly)
+        override this.Configure container = 
+            ignore()
+
+        static member start() = 
+            let apphost = new AppHost()
+            apphost.Init() 
+            
+            
+
