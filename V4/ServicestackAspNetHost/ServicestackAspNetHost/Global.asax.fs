f1d7ec75260bd631ec2ebefa1953e278eab810b6
Kunjan <kunjee17@gmail.com>
2014-02-10 14:28:55 +0530

    last finding added

--- V4/ServicestackAspNetHost/ServicestackAspNetHost/Global.asax.fs
+++ V4/ServicestackAspNetHost/ServicestackAspNetHost/Global.asax.fs
@@ -1,11 +1,16 @@
 ﻿namespace ServicestackAspNetHost
 
 open System
-
+(*
+--- Updated find out
+    If I comment apphost.Init() break point will hit application start on console.writeline. But if uncomment it, it is not working. 
+    So, here I am successfully creating instance of AppHost but busting it somewhere in Init.
+*)
 type Global() = 
     inherit System.Web.HttpApplication()
     let apphost = new App_Start.AppHost()
     member x.Application_Start (sender:Object, e:EventArgs) = 
+        Console.WriteLine("Just to put break point");
         apphost.Init() 
  
 
