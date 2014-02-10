3a9a42566957ba8f69dace76ea9d038c5e5c66d9
Kunjan <kunjee17@gmail.com>
2014-02-10 14:22:24 +0530

    demo updated with current suggestion

--- V4/ServicestackAspNetHost/ServicestackAspNetHost/Global.asax.fs
+++ V4/ServicestackAspNetHost/ServicestackAspNetHost/Global.asax.fs
@@ -4,8 +4,8 @@ open System
 
 type Global() = 
     inherit System.Web.HttpApplication()
-
+    let apphost = new App_Start.AppHost()
     member x.Application_Start (sender:Object, e:EventArgs) = 
-        App_Start.AppHost.start() 
+        apphost.Init() 
  
 
